bf0e4c91c71063d16c2401c5f05306cc1053dc2e
Halil Özel <halilozel1903@gmail.com>
2018-10-11 22:28:56 +0300

    Yorum satırları eklendi.

--- app/src/main/java/com/halil/ozel/rolldicegame/MainActivity.java
+++ app/src/main/java/com/halil/ozel/rolldicegame/MainActivity.java
@@ -11,13 +11,13 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final Random RANDOM = new Random();
+    public static final Random RANDOM = new Random(); // Random nesnesi olusturduk.
 
-    Button playGame;
+    Button playGame; // button
 
-    ImageView imgView1,imgView2;
+    ImageView imgView1,imgView2; // image
 
-    TextView textView2,textView3;
+    TextView textView2,textView3; // textview
 
 
     @Override
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // id değerleri getirildi.
         playGame = findViewById(R.id.playGame);
         imgView1 = findViewById(R.id.imgView1);
         imgView2 = findViewById(R.id.imgView2);
@@ -35,11 +36,14 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    // oyunu oynatma metodu
     public void playGame(View view) {
 
+        // value değerleri
         int value1 = randomDiceValues();
         int value2 = randomDiceValues();
 
+        // resim değerlerini alma.
         int picture1 = getResources().getIdentifier("dice_"+value1,"drawable",getPackageName());
         int picture2 = getResources().getIdentifier("dice_"+value2,"drawable",getPackageName());
 
@@ -47,18 +51,22 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        // resimleri yükleme
         imgView1.setImageResource(picture1);
         imgView2.setImageResource(picture2);
 
+
+        // sonucları gösterme
         textView2.setText(String.valueOf(value1));
         textView3.setText(String.valueOf(value2));
 
     }
 
+    // Random değer üreten fonksiyon
     public static int randomDiceValues(){
 
 
-        return RANDOM.nextInt(6) + 1;
+        return RANDOM.nextInt(6) + 1; // 1-6 arası
 
 
     }
